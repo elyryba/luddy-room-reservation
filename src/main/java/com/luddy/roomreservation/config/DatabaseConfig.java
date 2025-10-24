8e1410cfbba5ba496ca192f225b276585a15557a
Ely Ryba <elyryba@yahoo.com>
2025-10-24 01:01:04 -0400

    add database config

---
+++ src/main/java/com/luddy/roomreservation/config/DatabaseConfig.java
@@ -0,0 +1,21 @@
+﻿package com.luddy.roomreservation.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.data.relational.core.dialect.Dialect;
+import org.springframework.data.relational.core.dialect.SqliteDialect;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import javax.sql.DataSource;
+@Configuration
+public class DatabaseConfig extends AbstractJdbcConfiguration {
+    @Bean
+    public Dialect jdbcDialect(NamedParameterJdbcOperations operations) {
+        return SqliteDialect.INSTANCE;
+    }
+    @Bean
+    public TransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
